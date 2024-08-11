2ad88b61e4c8f2d5d252748ff6afec61253e6e3c
lanran <epiiplus@outlook.com>
2024-08-11 20:45:11 +0800

    security基础配置

---
+++ src/main/java/dev/example/entity/Rest.java
@@ -0,0 +1,20 @@
+package dev.example.entity;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+
+public record Rest<T>(int code, T data, String message) {
+    public static <T> Rest<T> success(T data) {
+        return new Rest<T>(200,data,"请求成功");
+    }
+    public static <T> Rest<T> success() {
+        return new Rest<T>(200,null,"请求成功");
+    }
+
+    public static <T> Rest<T> failure(int code, String message) {
+        return new Rest<T>(code,null,message);
+    }
+    public String asJsonString() {
+        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
+    }
+}
---
+++ src/test/java/dev/example/FullProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
